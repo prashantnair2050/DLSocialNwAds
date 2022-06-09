a57b781025145e6621375f93c4bfc0b6f6500070
Dr9 <Dr9@users.noreply.github.com>
2022-06-09 14:45:13 -0600

    Update app.py
    predict_classes was remanent of Iris categorical prediction.

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def predict():
     sal = float(request.form['sal'])
     feature = np.array([[age,sal]])
     stdFeatures = scFeatures.transform(feature)
-    predLabel = model.predict_classes(stdFeatures)
+    predLabel = model.predict(stdFeatures)
 
     return render_template('index.html', prediction_text='Given customer is a {} customer'.format('Good' if predLabel[0][0] == 1 else 'Bad'))
 
